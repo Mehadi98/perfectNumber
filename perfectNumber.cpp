3b7255b813be02247a7978dbc65e34bfa41280c4
Mehadi hasan <82320743+Mehadi98@users.noreply.github.com>
2022-10-11 02:05:40 -0700

    Add files via upload

---
+++ perfectNumber.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int number,sum;
+    // sum=0;
+    // cout<<"Enter Your Number :";
+    // cin>>number;
+    // for(int i=1;i<number;i++){
+    //     if(number%i==0){
+    //         sum+=i;
+    //     }
+    // }
+    // if (sum==number)
+    // {
+    //     cout<<number<<" is a Perfect Number";
+    // }
+    // else{
+    //     cout<<number<<" is not a Perfect Number";
+    // }
+    int number,sum;
+    sum=0;
+    cout<<"Enter Your Number :";
+    cin>>number;
+
+    for (int i=1;i<=number;i++){
+        sum=0;
+        for(int j=1;j<i;j++){
+            if (i%j==0)
+            {
+                sum+=j;
+            }
+        }
+        if (sum==i)
+        {
+            cout<<i<<endl;
+        }
+    }
+    
+    return 0;
+}
